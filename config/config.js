768f91456749723ebe7a24d7e540e741e97ef5e0
Jgouken <65487563+Jgouken@users.noreply.github.com>
2022-07-25 07:10:41 +0000

    config

--- config/config.js
+++ config/config.js
@@ -1,14 +1,15 @@
+const token = process.env['TOKEN']
 const Discord = require('discord.js');
 const Database = require("@replit/database")
 const db = new Database()
 const bot = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] })
 
 module.exports = {
-    Discord: Discord, 
-    bot: bot,
-		db: db,
-    
-    name: 'Make-A-Mand',
-    TOKEN: process.env.TOKEN,
-    prefix: `m-`
+	Discord: Discord,
+	bot: bot,
+	db: db,
+
+	name: 'Make-A-Mand',
+	TOKEN: token,
+	prefix: `m-`
 }
